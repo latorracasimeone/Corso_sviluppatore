59a08155dc8c04df14dd7301a7ad257d3b5d7d40
latorracasimeone <latorracasimeone@gmail.com>
2026-02-13 12:47:08 +0100

    fine capitolo 03

--- esercitazioni/esercitazione_20/Program.cs
+++ esercitazioni/esercitazione_20/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.WriteLine("Inserisci un numero intero:");
+string input = Console.ReadLine();
+int numero = int.Parse(input); // Conversione da stringa a intero
+Console.WriteLine($"Il numero inserito è: {numero}");
---
+++ esercitazioni2/esercitazione_22/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+// Acquisire l'input dell'utente
+Console.WriteLine("Inserisci un numero:");
+string input = Console.ReadLine();
+
+// Tentare di convertire l'input in un intero
+// Abbiamo bisogno di out ed int per poter raccogliere il risultato della conversione e sapere se è riuscita o meno
+// nel out dobbiamo specificare una variabile di tipo int che conterrà il risultato della conversione se questa ha successo
+// Se la conversione ha successo, viene creata una variabile numeroUtente che contiene il valore convertito
+// altrimenti numeroUtente avrà un valore di default (0) e la conversione fallirà restituendo false
+if (int.TryParse(input, out int numeroUtente))
+{
+    // Se la conversione ha successo, sommare il numero predefinito e l'input dell'utente
+    int risultato = numeroUtente;
+    Console.WriteLine($"Il numero è: {risultato}");
+}
+else
+{
+    // Se la conversione fallisce, stampare un messaggio di errore
+    Console.WriteLine("Errore: l'input inserito non è un numero valido.");
+}
