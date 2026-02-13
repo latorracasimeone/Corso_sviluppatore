d7f43235acb677d4f38fc3411771fe302117e257
latorracasimeone <latorracasimeone@gmail.com>
2026-02-13 12:00:29 +0100

    esercitazione Parse e Casting 03 in 18pt2

---
+++ esercitazioni/esercitazione_18pt2/Program.cs
@@ -0,0 +1,27 @@
+﻿// 1. Creazione della lista di stringhe
+List<string> listaNomi = new List<string>();
+ // 2. Chiedere all'utente di inserire il primo nome
+ Console.Write("Inserisci il primo nome:");
+string nome1 = Console.ReadLine();
+// Aggiungere il primo nome alla lista
+listaNomi.Add(nome1);
+// Chiedere all'utente di inserire il secondo nome
+Console.Write("Inserisci il secondo nome:");
+string nome2 = Console.ReadLine();
+// Aggiungere il secondo nome alla lista
+listaNomi.Add(nome2);
+// Chiedere all'utente di inserire il terzo nome
+Console.Write("Inserisci il terzo nome:");
+string nome3 = Console.ReadLine();
+// Aggiungere il terzo nome alla lista
+listaNomi.Add(nome3);
+// Stampare in console il secondo nome della lista
+Console.WriteLine("Il secondo nome inserito è:");
+
+//Chiedi all'utente quale nome vuole stampare
+Console.WriteLine("Quale nome vuoi stampare? (1°, 2° o 3°)?");
+int indice = int.Parse(Console.ReadLine());
+
+//Stampare il nome preselezionato dall'utente
+Console.WriteLine($"Il nome scelto è: {listaNomi[indice - 1]}");
+
