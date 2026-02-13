7f6c8fbc2f5d2af733a75e11df1abf3cef012460
latorracasimeone <latorracasimeone@gmail.com>
2026-02-13 09:49:33 +0100

    aggiunta esercitazione 13 e forse le altre non ancora utilizzate

---
+++ esercitazioni/esercitazione_13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Inserisci il tuo giorno di nascita:");
+
+string giorno = Console.ReadLine();
+
+Console.Write("Inserisci il tuo mese di nascita in numeri:");
+
+string mese = Console.ReadLine();
+
+Console.Write("Inserisci il tuo anno di nascita:");
+
+string anno = Console.ReadLine();
+
+Console.WriteLine($"Essio Anna Azzo Exy, t.  {giorno}/{mese}/{anno}!");
