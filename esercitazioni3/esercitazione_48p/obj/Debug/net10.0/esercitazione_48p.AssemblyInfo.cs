72369e6bde26ebc425625ac1d7dfbc523323084c
latorracasimeone <latorracasimeone@gmail.com>
2026-02-18 23:51:58 +0100

    aggiornamento 18/02 23:51

--- esercitazioni3/esercitazione_48p/obj/Debug/net10.0/esercitazione_48p.AssemblyInfo.cs
+++ esercitazioni3/esercitazione_48p/obj/Debug/net10.0/esercitazione_48p.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("esercitazione_48p")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24110625ae7af6334d3c6e4a4ea61128765b0224")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d51ddb4b2f7ec314432df9b594d7fec772b6ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("esercitazione_48p")]
 [assembly: System.Reflection.AssemblyTitleAttribute("esercitazione_48p")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ esercitazioni3/esercitazione_49/Program.cs
@@ -0,0 +1,29 @@
+﻿//lezione 10 Metodi Lista
+List<int> numeri = new List<int>();
+numeri.Add(1);
+numeri.Add(2);
+numeri.Add(3);
+Console.WriteLine(string.Join(", ", numeri)); // output: 1, 2, 3
+
+
+Console.WriteLine(numeri.Count); // output: 3
+
+numeri.Add(4);
+
+Console.WriteLine(string.Join(", ", numeri));
+
+
+// aggiunta diretta di più elementi alla lista
+numeri.AddRange(7,8,9,10);
+
+// aggiunta di più elementi alla lista tramite un array
+numeri.AddRange(new int[] { 4, 5, 6 });
+
+Console.WriteLine(string.Join(", ", numeri)); // output: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+// oppure tramite un altra lista
+List<int> numeri2 = new List<int>() {11, 12, 13, 14 };
+numeri.AddRange(numeri2);
+Console.WriteLine(string.Join(", ", numeri)); // output: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+
+
