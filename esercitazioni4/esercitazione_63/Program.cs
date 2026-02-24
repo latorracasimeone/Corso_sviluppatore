7c70c21b1fec9a814c84dbc74eff76898bdafcca
latorracasimeone <latorracasimeone@gmail.com>
2026-02-24 12:47:45 +0100

    GUARDA README agg lez 14

--- esercitazioni4/esercitazione_63/Program.cs
+++ esercitazioni4/esercitazione_63/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("puppa!");
+﻿//LEZIONE 14 Metodi Files Folder
+
+
+// Esempio di percorso assoluto:   (quando scrivo //// significa che son barre di codice da esempio ma che non devono influire nel codice complessivo)
+////string absolutePath = @"C:\Users\Username\Documents\file.txt";
+
+//prova percorso relativo
+string relativePath = @"..\..\testo di prova";
