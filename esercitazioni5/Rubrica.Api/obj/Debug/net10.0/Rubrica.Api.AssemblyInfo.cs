47f65ff8b2418bb23a272c5cf6111aa04cc6a64b
latorracasimeone <latorracasimeone@gmail.com>
2026-03-17 12:53:39 +0100

    dddd

--- esercitazioni5/Rubrica.Api/obj/Debug/net10.0/Rubrica.Api.AssemblyInfo.cs
+++ esercitazioni5/Rubrica.Api/obj/Debug/net10.0/Rubrica.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rubrica.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8d0437a11bab6794bde1966f0dbb4244d95f55b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4edcedbace50a90f187282b818afed1bfc5d0475")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rubrica.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rubrica.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
